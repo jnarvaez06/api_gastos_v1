9832f5979eede9604fbf70c0e1949ceb2b445732
jnarvaez06 <57082568+jnarvaez06@users.noreply.github.com>
2024-02-13 21:13:51 -0500

    Update gitPull.sh

--- gitPull.sh
+++ gitPull.sh
@@ -2,3 +2,15 @@
 
 # Cambia al directorio donde está tu repositorio
 cd /var/www/html/api_gastos_v1
+
+# Realiza un pull desde la rama main
+git pull origin main
+
+# Reinicia tu aplicación si es necesario
+# Esto podría variar dependiendo de tu aplicación y cómo esté configurada
+# Por ejemplo, si estás utilizando Node.js, podrías necesitar reiniciar el servidor
+# con el siguiente comando:
+# pm2 restart <nombre_del_proceso>
+
+# Ejecuta otros comandos de post-despliegue si es necesario
+# Por ejemplo, instalar nuevas dependencias, compilar assets, etc.
