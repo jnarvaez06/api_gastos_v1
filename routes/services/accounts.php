50bafa5f7a70429f4b0ccc07450815fa6780da69
julian-narvaez <benxiin215@gmail.com>
2024-02-22 23:33:57 -0500

    correccion error accounts.php

--- routes/services/accounts.php
+++ routes/services/accounts.php
@@ -4,7 +4,7 @@ require_once "controllers/accounts_controller.php";
 $gActions = new GeneralActions;
 
 $data = new AccountsController();
-$response = $data->getData($method,$POST);
+$response = $data->getData($method,$_POST);
 
 $gActions->emitResponse(200,"Success",array('key'=>'data','value'=>$response));
 
