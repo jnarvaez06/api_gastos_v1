008661b52dc88cf1d049f29c45517df3c1360d81
julian-narvaez <benxiin215@gmail.com>
2024-03-08 21:35:18 -0500

    prueba para consumir flutter

--- routes/routes.php
+++ routes/routes.php
@@ -15,7 +15,7 @@ $method =  $routes[0];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 if ($requestMethod=='POST' && empty($_POST)) {
-    $gActions->emitResponse(400, 'Error', array('key'=>'message', 'value'=>"Empty Post data.")); 
+    $gActions->emitResponse(400, 'Error', array('key'=>'message', 'value'=>"Empty Post data...")); 
     return;
 }
 
